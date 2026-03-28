45d00e46c12b9fe64d219f02f3680c4db43cdd5e
Jean-Michel Friedt <friedtj@free.fr>
2026-03-28 14:33:44 +0100

    MAX2771 experiment

---
+++ max2771/README.md
@@ -0,0 +1,59 @@
+# NISAR reception from PocketSDR clone with two MAX2771
+
+```
+sudo PocketSDR/app/pocket_conf/pocket_conf ./pocket_NISAR_6MHz.conf 
+sudo PocketSDR/app/pocket_conf/pocket_conf 
+#  [CH1] F_LO =  1229.025000 MHz, F_ADC = 24.000000 MHz (IQ), F_FILT =  0.0 MHz, BW_FILT = 23.4 MHz
+#  [CH2] F_LO =  1229.025000 MHz, F_ADC =  0.000000 MHz (IQ), F_FILT =  0.0 MHz, BW_FILT = 23.4 MHz
+```
+
+```
+sudo PocketSDR/app/pocket_dump/pocket_dump /tmp/1.bin /tmp/2.bin
+```
+The recording only started a few seconds before the predicted maximum elevation 
+
+<img src="260326_schedule.png">
+
+```
+$ stat /tmp/1.bin
+  File: /tmp/1.bin
+  Size: 2142240768      Blocks: 4184064    IO Block: 4096   regular file
+Device: 0,39    Inode: 180679      Links: 1
+Access: (0644/-rw-r--r--)  Uid: (    0/    root)   Gid: (    0/    root)
+Access: 2026-03-26 20:03:05.885563149 +0100
+Modify: 2026-03-26 19:51:43.824067742 +0100
+Change: 2026-03-26 19:51:43.824067742 +0100
+ Birth: 2026-03-26 19:50:59.190699895 +0100
+$ stat /tmp/2.bin
+  File: /tmp/2.bin
+  Size: 2142240768      Blocks: 4184064    IO Block: 4096   regular file
+Device: 0,39    Inode: 180680      Links: 1
+Access: (0644/-rw-r--r--)  Uid: (    0/    root)   Gid: (    0/    root)
+Access: 2026-03-26 19:50:59.190699895 +0100
+Modify: 2026-03-26 19:51:43.824067742 +0100
+Change: 2026-03-26 19:51:43.824067742 +0100
+ Birth: 2026-03-26 19:50:59.190699895 +0100
+```
+
+but still allowed to collect useful data on the surveillance and 
+reference channels.
+
+Because the MAX2771 provides the complex conjugate of the IQ stream (i.e. I-jQ)
+the original image was upside down when interpreting the file record content
+as I+jQ
+
+<img src="XYcomplex_conj.png">
+
+and flipped after correcting for I-jQ
+
+<img src="XY.png">
+
+Projecting on the map+DEM validates the source of the reflections
+
+<img src="XY_map_DEM.png">
+
+Since the MAX2771 samples with low resolution ADCs, attempts were made
+to replace the reference channel with synthetic chirps, requiring to match
+the time delay and phase of the observed signal:
+
+<img src="XY_synthetic_windowing.png">
---
+++ max2771/nisar_process6.m
@@ -0,0 +1,162 @@
+clc;
+clear;
+close all;
+pkg load signal
+
+load kpos
+
+%% Parameters
+c = 3e8;
+fc = 1229e6; % c/lembda;
+lembda = c/fc; % 24e-2;
+
+H = 750.2e3;       % TLE Celestrak (semi-major axis)
+theta0 = 44*pi/180;
+beta_sat = -H/sin(theta0);
+
+fs = 24e6;
+
+Nr = 2501;
+freq = (-(Nr-1)/2:(Nr-1)/2).'/Nr*fs;
+
+orbits_per_day=14.42502395
+orbit_duration=24*3600/orbits_per_day; % second
+Rearth=6371*1e3;                       % meters
+orbit_length=(Rearth+H)*2*pi;
+
+linspeed=orbit_length/orbit_duration
+% linspeed = sqrt(5.972e24*6.67430e-11/(6371e3+H)) % sqrt((G*M)/(R+H)) gravity constant x Earth mass
+P = 3501;
+% experimentall tuned t to match
+% synthetic=[ch zeros(1,length(Ssur(:,1000))-length(ch))];
+% plot(real(synthetic));hold on
+% plot(real(Sref(:,1000)))
+
+%%
+%rr=1;
+%for m=200:300
+%  t=[-m:m]/fs;
+%  ch=chirp(t,0,t(end),10e6)-j*chirp(t,0,t(end),10e6);
+%  maxval(rr)=max(abs(xcorr(ch,(Sref(:,1000)))));
+%  rr=rr+1;
+%end
+%subplot(211);plot([200:300],maxval);xlabel('m (samples)');ylabel('max(xcorr)')
+%%
+
+t=[-248:248]/fs;  % 20 us pulsewidth according https://www.eoportal.org/satellite-missions/nisar#sweepsar-discussion "the 20 MHz pulse extent is 20 ms, while the 5 MHz pulse extent is 5 µs, giving a total pulse extent of 25 µs"
+ch=chirp(t,0,t(end),10e6)-j*chirp(t,0,t(end),10e6);
+ch=(ch).*([hamming(120)(1:60) ; ones(length(ch)-120,1) ; hamming(120)(61:end)]');
+
+%% Load data
+f1=fopen('2.bin');  % ref
+f2=fopen('1.bin');  % sur
+clear d
+
+%% Find pulses
+% p1 = find(abs(ref1)>70);
+% p2 = p1(1);
+% pindx = p2;
+% do_search = 1;
+% while do_search
+%     ref2 = ref1(p2+200:end);
+%     p1 = find(abs(ref2)>70)+p2+200-1;
+%     if ~isempty(p1)
+%         p2 = p1(1);
+%         pindx = [pindx;p2]; %#ok<AGROW>
+%     else
+%         do_search = 0;
+%     end
+% end
+%pindx=kpos(1000:end-1000);
+
+%% Raw data
+ Sref = zeros(Nr,P);
+ Ssur = zeros(Nr,P);
+ L = 5;
+ AS = 50;
+
+pindx=kpos(8000:8000+P+L);
+
+ fseek(f1,2*pindx(1)-AS-1-L);  % complex char = 2x2
+ fseek(f2,2*pindx(1)-AS-1-L);  % complex char = 2x2
+ d=fread(f1,(pindx(end)-pindx(1)+Nr+L)*2,'int8');ref1=d(1:2:end)-j*d(2:2:end);
+ d=fread(f2,(pindx(end)-pindx(1)+Nr+L)*2,'int8');sur1=d(1:2:end)-j*d(2:2:end);
+ pindx=pindx-(pindx(1)-AS)+1+L;
+ clear d
+
+ for p = 1:P
+     disp(p);
+     Sref(:,p) = ref1(pindx(p)-AS:pindx(p)-AS+Nr-1);
+     Sls = zeros(Nr,L);
+     for l = -(L-1)/2:(L-1)/2
+         Sls(:,l+(L-1)/2+1) = ref1(pindx(p)-AS+l:pindx(p)-AS+Nr-1+l);
+     end
+     temp = sur1(pindx(p)-AS:pindx(p)-AS+Nr-1);
+     Ssur(:,p) = temp-Sls*pinv(Sls)*temp;
+ end
+ N = length(ref1);
+ t = (0:N-1)/fs;
+ t1 = t(1:end);
+
+ t0 = t1(round(pindx(1:end-3))-round(AS)).';t0 = t0(1:P);
+ t0 = t0-(t0(1)+t0(end))/2;
+% load t0.mat t0;
+
+ Sref_fft = zeros(Nr,P);
+ Ssur_fft = zeros(Nr,P);
+ S = zeros(Nr,P);
+ for p = 1:P
+     x=xcorr(ch,Sref(:,p));
+     [~,maxpos(p)]=max(abs(x));
+     synthphase(p)=arg(x(maxpos(p)));
+     synthetic=zeros(1,length(Ssur(:,p)));
+     synthetic(2492-maxpos(p)+1:2492-maxpos(p)+length(ch))=ch*exp(-j*synthphase(p));
+%     Sref_fft(:,p) = fftshift(fft(Sref(:,p)-mean(Sref(:,p))));
+     Sref_fft(:,p) = fftshift(fft(synthetic));
+     Ssur_fft(:,p) = fftshift(fft(Ssur(:,p)-mean(Ssur(:,p))));
+     S(:,p) = Ssur_fft(:,p).*conj(Sref_fft(:,p));
+ end
+
+%load S.mat S;
+
+%% SAR imaging
+al_sat = linspeed*t0;
+
+beta_I = linspace(0,25000,2501);nbe = length(beta_I);
+F1 = exp(-1j*2*pi*freq*beta_I/c)/sqrt(Nr);
+
+al_I = linspace(-5e-3,5e-3,2001);nal = length(al_I);
+F2 = exp(-1j*2*pi*al_sat*al_I/lembda)/sqrt(P);
+
+Image_I = F1'*S*conj(F2);
+Image_I_db = 10*log10(abs(Image_I)/max(abs(Image_I(:))));
+figure;imagesc(al_I,beta_I,Image_I_db);axis xy;
+colormap(jet);colorbar;clim([-30,0]);
+xlabel('\alpha_{\itI}');ylabel('\beta_{\itI} (m)');
+ylabel(colorbar,'Normalized amplitude (dB)');
+%set(gca,'FontName','Times New Roman','FontSize',14);
+
+%% Image on x-o-y plane
+xm = linspace(0,10e3,2001);nxm = length(xm);
+ym = linspace(-4000,4000,2001);nym = length(ym);
+
+[X,Y] = meshgrid(xm,ym.');A = Y;
+
+tmpA = Y.^2;
+tmpB = H/sin(theta0);
+tmpC = sqrt((H*cot(theta0)+X).^2+Y.^2+H^2)+sqrt(X.^2+Y.^2);
+B1 = -sqrt(4*tmpA.*tmpB.^2.*tmpC.^2-4*tmpA.*tmpC.^4+tmpB.^4.*tmpC.^2-2*tmpB.^2.*tmpC.^4+tmpC.^6)-tmpB.^3+tmpB.*tmpC.^2;
+B2 = 2*(tmpB.^2-tmpC.^2);
+B = B1./B2;
+
+[al_I_grid,beta_I_grid] = meshgrid(al_I,beta_I.');
+al_grid = A./(beta_sat-B);
+beta_grid = sqrt(A.^2+B.^2)+B-A.^2./(beta_sat-B)/2;
+
+Image_xy = interp2(al_I_grid,beta_I_grid,Image_I,al_grid,beta_grid,'linear',0);
+Image_xy_db = 10*log10(abs(Image_xy)/max(abs(Image_xy(:))));
+figure;imagesc(ym,xm,Image_xy_db.');axis xy;
+clim([-30,0]);colormap(jet);colorbar;
+xlabel('x (m)');ylabel('y (m)');
+ylabel(colorbar,'Normalized amplitude (dB)');
+%set(gca,'FontName','Times New Roman','FontSize',14);
